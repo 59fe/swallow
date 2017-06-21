71f40ad6bf85b72ec3a993c4b3cee40efd8f2626
柏舟 <wanggang@59store.com>
2017-06-21 10:33:48 +0800

    文章页面完善

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import style from './style.scss'
 import LzEditor from 'react-lz-editor'
 
+const uploadConfig = {
+    QINIU_URL: "http://up.qiniu.com", //上传地址，现在暂只支持七牛上传
+    QINIU_IMG_TOKEN_URL: "/api/qiniu_token", //请求图片的token
+    QINIU_PFOP: {
+        url: "/api/qiniu_files" //七牛持久保存请求地址
+    },
+    QINIU_VIDEO_TOKEN_URL: "/api/qiniu_token", //请求媒体资源的token
+    QINIU_FILE_TOKEN_URL: "/api/qiniu_token", //其他资源的token的获取
+    QINIU_IMG_DOMAIN_URL: "http://fecdn.59store.com/", //图片文件地址的前缀
+    QINIU_DOMAIN_VIDEO_URL: "http://fecdn.59store.com/", //视频文件地址的前缀
+    QINIU_DOMAIN_FILE_URL: "http://fecdn.59store.com/" //其他文件地址前缀
+}
+
 export default class Article extends React.Component {
 
     constructor (props) {
@@ -16,7 +29,7 @@ export default class Article extends React.Component {
 
         if (!this.state.contentInitialed) {
             this.setState({
-                content: this.props.initialContent
+                content: this.props.initialContent.split('<!--EDITOR_CONTENT-->')[1] || ''
             })
         }
 
@@ -44,8 +57,10 @@ export default class Article extends React.Component {
                         importContent={this.state.content}
                         cbReceiver={::this.receiveHtml}
                         fullScreen={false}
-                        image={false}
-                        video={false}
+                        image={true}
+                        audio={true}
+                        video={true}
+                        uploadConfig={uploadConfig}
                         convertFormat="html"
                     />
                 </div>
--- src/templates/article.tpl
+++ src/templates/article.tpl
@@ -19,11 +19,29 @@ body{
     min-height: 100%;
     overflow: hidden;
 }
+video, audio{
+    max-width: 100%;
+    height: auto;
+}
 </style>
 </head>
 <body>
 <div class="container">
 <h1 class="title"><%=title%></h1>
-<div class="content"><%=html%></div>
+<div class="content">
+<!--EDITOR_CONTENT-->
+<%=html%>
+<!--EDITOR_CONTENT-->
 </div>
+</div>
+<script>
+var videos = document.querySelectorAll('video');
+var audios = document.querySelectorAll('audio');
+[].forEach.call(videos, function(video) {
+    video.setAttribute('controls', 'controls');
+});
+[].forEach.call(audios, function(audio) {
+    audio.setAttribute('controls', 'controls');
+});
+</script>
 </html>
