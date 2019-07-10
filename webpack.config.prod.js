6d03d17e629e5ed676a58721cc10dcdd80e6fcaf
半边 <zangt@qeek.cc>
2019-07-10 20:06:41 +0800

    no scheme

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -21,7 +21,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, './dist'),
         filename: '[name].[chunkhash:5].js',
-        publicPath: 'http://fecdn.qeebike.com/repo/swallow/'
+        publicPath: '//fecdn.qeebike.com/repo/swallow/'
     },
     module: {
         loaders: [
